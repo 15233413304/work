affaa90c381fb670095e704b1a10e235e8fda1fe
XuHaoran <xhr@XuHaorandeMBP.mshome.net>
2022-11-10 13:55:21 +0800

    delete 清理

--- 2006B/day3/index.js
+++
@@ -1,5 +0,0 @@
-import App from './index.vue'
-new Vue({
-    el: '#app',
-    render: h => h(App)
-})
--- 2006B/day4/webpack-advanced/src/index.js
+++
@@ -1,6 +0,0 @@
-import './index.css'
-import './index.vue'
-let sum = function (a,b){
-    return a + b
-}
-console.log(sum(5,5))
